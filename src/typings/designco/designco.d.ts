8aa119190a746c0e0b4bb9d2569f04dc542de508
Carl <carl@carlossus.com>
2015-05-22 18:08:43 +0800

    Adding designco typings file to repo

---
+++ src/typings/designco/designco.d.ts
@@ -0,0 +1,15 @@
+export module Designco {
+	export interface User {
+		username: string;
+		email: string;
+		displayName: string;
+		status: UserStatus;
+		password: string;
+	}
+	
+	export const enum UserStatus {
+		Inactive,
+		Active,
+		Pending
+	}
+}
