68c1893f7a19ca58fe021ecc12a31d7d2e06b285
Carl <carl@carlossus.com>
2015-05-23 19:18:05 +0800

    Added npm script 'start'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 var hapi = require("hapi");
+var log = require("./logger");
+//TODO: Put port in config
+var port = 45199;
 var server = new hapi.Server();
 server.connection({
-    port: 45199
+    port: port
 });
 server.start(function () {
+    log.info("Starting server on port " + port);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import hapi = require("hapi");
+import log = require("./logger");
 
+//TODO: Put port in config
+var port = 45199;
 var server = new hapi.Server();
 server.connection({
-	port: 45199
+	port: port
 });
 
 server.start(() => {
-	
+	log.info("Starting server on port " + port);
 });
