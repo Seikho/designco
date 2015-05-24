d15cac59d7f5d822e89c3a8a5836b320ccb7b07c
Carl <carl@carlossus.com>
2015-05-24 16:54:08 +0800

    Ignoring .log files. npm start launches node in harmony mode for es6 features

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 var hapi = require("hapi");
 var log = require("./logger");
+
 //TODO: Put port in config
 var port = 45199;
 var server = new hapi.Server();
+
 server.connection({
     port: port
 });
+
 server.start(function () {
     log.info("Starting server on port " + port);
 });
