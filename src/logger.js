5f28419d5c18716c2a277b218d69c41fa7db7c14
Carl <carl@carlossus.com>
2015-05-25 19:40:36 +0800

    Logger: Tidy up exports from TS transpile

--- src/logger.js
+++ src/logger.js
@@ -2,20 +2,27 @@ var chalk = require("chalk");
 function info(message) {
     log("INFO", message, chalk.blue);
 }
-exports.info = info;
+
 function warn(message) {
     log("WARN", message, chalk.yellow);
 }
-exports.warn = warn;
+
 function error(message) {
     log("ERROR", message, chalk.red);
 }
-exports.error = error;
+
 function debug(message) {
     log("DEBUG", message, chalk.gray);
 }
-exports.debug = debug;
+
 function log(prefix, message, colour) {
     var timestamp = new Date().toLocaleTimeString().slice(0, 8);
     console.log("[%s] %s: %s", timestamp, colour(prefix), message);
+}
+
+module.exports = {
+    info: info,
+    warn: warn,
+    error: error,
+    debug: debug
 }
