2a646220fb82f68842c59c1dd8e5cc73ad704abf
Carl <carl@carlossus.com>
2015-06-06 11:01:55 +0800

    Converted index to typescript. Added hapi typings

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
-var hapi = require("hapi");
-var log = require("./logger");
+import hapi = require("hapi");
+import log = require("./logger");
+
 //TODO: Put port in config
 var port = 45199;
 var server = new hapi.Server();
+
 server.connection({
     port: port
 });
-server.start(function () {
+
+server.start(() => {
     log.info("Starting server on port " + port);
 });
--- src/typings/tsd.d.ts
+++ src/typings/tsd.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="kafka-node/kafka-node.d.ts" />
 /// <reference path="chalk/chalk.d.ts" />
+/// <reference path="hapi/hapi.d.ts" />
