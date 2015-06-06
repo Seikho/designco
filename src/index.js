1781e89441971714516ac0e2c9ffc2c6b5a73cae
Carl <carl@carlossus.com>
2015-06-06 12:37:40 +0800

    Removed kafka typings

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ global.publish = pub;
 global.log = log;
 global.config = {
     port: 45199,
-    redisUrl: "192.168.59.103:2181"
+    redisUrl: "192.168.59.103:6379"
 };
 //TODO: Put port in config
 var port = global.config.port || 45199;
