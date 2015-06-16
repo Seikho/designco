b34560d01c168b36de1aef5c95b0863016496bd3
Carl <carl@carlossus.com>
2015-06-16 19:19:35 +0800

    moving socket server and web server to separate modules

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ var store = require("ls-events");
 store.setHost("192.168.59.103", 6379);
 var dbInit = require("./store/init");
 var path = require("path");
-var hapi = require("hapi");
 var log = require("ls-logger");
 var cfg = require("ls-config");
-var io = require("socket.io");
 require("./handlers/users/create");
 var basePath = path.resolve(__dirname, "..");
 var liveDb = path.join(basePath, "designco.db");
@@ -15,35 +13,7 @@ cfg.config("eventsPort", 10001);
 cfg.config("liveDatabase", "designco.db");
 cfg.config("baseDatabase", "designco-base.sqlite");
 dbInit();
-//TODO: Put port in config
-var server = new hapi.Server();
-// Web API listener
-server.connection({
-    port: cfg.config("webPort"),
-    labels: "web"
-});
-// Events API listener
-server.connection({
-    port: cfg.config("eventsPort"),
-    labels: "events"
-});
 // Attach socket.io and store the server in config
-var ioServer = io(server.select("events").listener);
-cfg.config("io", ioServer);
-server.start(function () {
-    log.info("Starting server on port " + cfg.config("webPort"));
-});
-ioServer.on("connection", function (socket) {
-    log.info("[CONNECTED] " + socket.id);
-    socket.on("subscribe", function (msg) {
-        log.info("[SOCKET:SUB] " + socket.id + ": " + msg);
-        var options = JSON.parse(msg);
-        // Subscribe the socket to the requested channel
-        store.psub(options.channel, function (ch, pt, msg) {
-            socket.emit(ch, msg);
-        });
-    });
-});
 // Pub/sub test code
 var testCode = function () {
     store.client().flushdb([], function (err, succ) {
@@ -60,4 +30,4 @@ var testCode = function () {
         store.pub(testEvent);
     });
 };
-setTimeout(testCode, 1000);
+setTimeout(testCode, 10000);
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,9 @@ store.setHost("192.168.59.103", 6379);
 
 import dbInit = require("./store/init");
 import path = require("path");
-import hapi = require("hapi");
 import log = require("ls-logger");
 import cfg = require("ls-config");
-import io = require("socket.io");
+
 require("./handlers/users/create");
 
 var basePath = path.resolve(__dirname, "..");
@@ -20,43 +19,14 @@ cfg.config("baseDatabase", "designco-base.sqlite");
 
 dbInit();
 
-//TODO: Put port in config
-var server = new hapi.Server();
 
-// Web API listener
-server.connection({
-    port: cfg.config("webPort"),
-    labels: "web"
-});
-
-// Events API listener
-server.connection({
-    port: cfg.config("eventsPort"),
-    labels: "events"
-});
 
 // Attach socket.io and store the server in config
-var ioServer = io(server.select("events").listener);
-cfg.config("io", ioServer);
 
-server.start(() => {
-    log.info("Starting server on port " + cfg.config("webPort"));
-});
 
-ioServer.on("connection", socket => {
-    log.info("[CONNECTED] " + socket.id);
-    socket.on("subscribe", msg => {
-        log.info("[SOCKET:SUB] " + socket.id + ": " + msg);
-        
-        var options = JSON.parse(msg);
-        
-        // Subscribe the socket to the requested channel
-        store.psub(options.channel, (ch, pt, msg) => {
-            socket.emit(ch, msg);
-        });
-    });
-    
-});
+
+
+
 
 // Pub/sub test code
 var testCode = () => {
@@ -75,7 +45,7 @@ var testCode = () => {
     });
 };
 
-setTimeout(testCode, 1000);
+setTimeout(testCode, 10000);
 
 
 
