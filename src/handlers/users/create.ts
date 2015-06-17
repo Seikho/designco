4070dcfb6034ec323e8b0cb641937be8d974ac80
Carl <carl@carlossus.com>
2015-06-17 18:49:40 +0800

    'username' in users table now PK

--- src/handlers/users/create.ts
+++ src/handlers/users/create.ts
@@ -9,6 +9,10 @@ store.psub("users/create/*", createUserHandler);
 function createUserHandler(channel: string, pattern: string, message: string) {
 	var user: App.User = JSON.parse(message);
 	log.info("[HANDLER] Create request '" + user.username + "'");
+	
+	// If the user isn't valid, do not continue
+	if (!isValidUser(user)) return;
+	 
 	getUserCreatedEvent(user).then((prevEvent: any[]) => {
 
 		var alreadyExists = prevEvent.length !== 0;
@@ -39,4 +43,25 @@ function createUser(user: App.User) { // Return a promise with the result
 
 function userAlreadyExists(user: App.User) {
 	log.error("[USER:CREATE] Failed to create '" + user.username + "': Already exists");
+}
+
+function insertUser(user: App.User) {
+	db("users").insert(user);
+}
+
+function isValidUser(user: App.User) {
+	var requiredFields = [
+		"displayName",
+		"username",
+		"email",
+		"password",
+		"enabled",
+		"company"
+	];
+	
+	var isAnyNull = requiredFields.some(field => {
+		return !!user["field"];
+	});
+	
+	return isAnyNull;
 }
