3bbf8561d78897bdc2f8b501e5fbdb6bb5d2940f
Carl <carl@carlossus.com>
2015-06-19 17:50:33 +0800

    Removed debug statement from static route

--- src/webapi/static.js
+++ src/webapi/static.js
@@ -1,6 +1,5 @@
 var path = require("path");
 var staticPath = path.join(path.resolve(__dirname, "../../"), "front");
-console.log(staticPath);
 var staticRoute = {
     method: "GET",
     path: "/{param*}",
--- src/webapi/static.ts
+++ src/webapi/static.ts
@@ -2,7 +2,6 @@ import path = require("path");
 export = staticRoute;
 
 var staticPath = path.join(path.resolve(__dirname, "../../"), "front");
-console.log(staticPath);
 
 var staticRoute = {
 	method: "GET",
