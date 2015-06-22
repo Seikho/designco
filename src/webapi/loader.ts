c06f078fdedd9cd09153df076093aae677d60241
Carl <carl@carlossus.com>
2015-06-22 18:57:06 +0800

    Removed 'applyRoutes' from webapi loader

--- src/webapi/loader.ts
+++ src/webapi/loader.ts
@@ -2,10 +2,3 @@ import server = require("../server");
 import staticRoute = require("./static");
 
 server.route(staticRoute);
-
-function applyRoutes(routes: {}) {
-	for (var r in routes) {
-		var route = routes[r];
-		server.route(r);
-	}
-}
