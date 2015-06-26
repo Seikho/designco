0f075a984e5cc25a7e2b05985d2aa69dcb3ddf66
Carl <carl@carlossus.com>
2015-06-26 17:58:44 +0800

    Added 'request' to project dependencies

--- src/api/users/auth.js
+++ src/api/users/auth.js
@@ -1,20 +1,11 @@
-var db = require("../../store/db");
 var Promise = require("bluebird");
+var cfg = require("ls-config");
 function login(login) {
     if (!isValidRequest(login))
         return Promise.resolve(false);
-    getUser(login.username)
-        .then(function (user) { return loginHandler(user, login.password); });
-    // then create session
 }
-function getUser(username) {
-    return db("users")
-        .select("password")
-        .where({ username: username });
-}
-function loginHandler(user, password) {
-    var isCorrect = user.password === password;
-    return Promise.resolve(isCorrect);
+function authHost() {
+    return "http://localhost:" + cfg.config("authPort");
 }
 function isValidRequest(login) {
     var isFieldsMissing = (login.username == null || login.password == null);
--- src/api/users/auth.ts
+++ src/api/users/auth.ts
@@ -1,23 +1,16 @@
 import db = require("../../store/db");
 import Promise = require("bluebird");
+import request = require("request");
+import cfg = require("ls-config");
 
 function login(login: App.Login) {
 	if (!isValidRequest(login)) return Promise.resolve(false);
-	
-	getUser(login.username)
-		.then(user => loginHandler(user, login.password))
-		// then create session
+
+
 }
 
-function getUser(username: string) {
-	return db("users")
-		.select("password")
-		.where({ username: username });
-}
-
-function loginHandler(user: { password: string }, password: string) {
-	var isCorrect = user.password === password;
-	return Promise.resolve(isCorrect);
+function authHost() {
+	return "http://localhost:" + cfg.config("authPort");
 }
 
 function isValidRequest(login: App.Login) {
