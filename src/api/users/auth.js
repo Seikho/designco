5e05d41602fbfe5f028f8f1ae89a8a0a137a8791
Carl <carl@carlossus.com>
2015-06-26 18:03:13 +0800

    users/auth now hits auth-api 'authenticate' endpoint

--- src/api/users/auth.js
+++ src/api/users/auth.js
@@ -1,11 +1,23 @@
 var Promise = require("bluebird");
+var request = require("request");
 var cfg = require("ls-config");
 function login(login) {
     if (!isValidRequest(login))
         return Promise.resolve(false);
+    var promise = new Promise(function (resolve, reject) {
+        var handler = function (error, response, body) {
+            if (error)
+                return reject(error);
+            resolve(body);
+        };
+        request.post(authHost(), login, handler);
+    });
+    return promise;
+}
+function requestHandler(error, response, body) {
 }
 function authHost() {
-    return "http://localhost:" + cfg.config("authPort");
+    return "http://localhost:" + cfg.config("authPort") + "/authenticate";
 }
 function isValidRequest(login) {
     var isFieldsMissing = (login.username == null || login.password == null);
--- src/api/users/auth.ts
+++ src/api/users/auth.ts
@@ -3,17 +3,30 @@ import Promise = require("bluebird");
 import request = require("request");
 import cfg = require("ls-config");
 
-function login(login: App.Login) {
-	if (!isValidRequest(login)) return Promise.resolve(false);
+function login(login: App.Login): Promise<boolean> {
+    if (!isValidRequest(login)) return Promise.resolve(false);
 
+    var promise = new Promise<boolean>((resolve, reject) => {
+        var handler = (error, response, body) => {
+			if (error) return reject(error);
+			resolve(body);
+        };
+
+		request.post(authHost(), login, handler);
+    });
+
+	return promise;
+}
+
+function requestHandler(error, response, body) {
 
 }
 
 function authHost() {
-	return "http://localhost:" + cfg.config("authPort");
+    return "http://localhost:" + cfg.config("authPort") + "/authenticate";
 }
 
 function isValidRequest(login: App.Login) {
-	var isFieldsMissing = (login.username == null || login.password == null);
-	return isFieldsMissing;
+    var isFieldsMissing = (login.username == null || login.password == null);
+    return isFieldsMissing;
 }
