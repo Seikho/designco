c7f083e33ee6b0aff90f111f9128fdda1f97a967
Carl <carl@carlossus.com>
2015-06-28 16:43:09 +0800

    Nav controller now responsible for changing views

--- front/components/nav/controller.js
+++ front/components/nav/controller.js
@@ -1,12 +1,20 @@
 export class NavController {
 	constructor() {
+		this.self = this;
+
         this.menuItems = [
-            { title: 'signs' },
-            { title: 'banners' },
-            { title: 'vehicles' },
-            { title: 'print' },
-            { title: 'fabricated' },
-            { title: 'traditional' }
+            { title: 'Store', view: "store" },
+            { title: 'Banners', view: "banners" },
+            { title: 'Vehicles', view: "vehicles" },
+            { title: 'Print', view: "print" },
+            { title: 'Fabricated', view: "fabricated" },
+            { title: 'Traditional', view: "traditional" }
         ];
+
+		this.currentView = this.menuItems[0];
+
+		this.changeView = newView => {
+			this.currentView = newView;
+		}
 	}
 }
