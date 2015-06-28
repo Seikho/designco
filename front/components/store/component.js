fd00c701c6b385e0a4ab6bedb0a25d4d26a7322d
Carl <carl@carlossus.com>
2015-06-28 18:42:04 +0800

    Reverted state.go routing changes

--- front/components/store/component.js
+++ front/components/store/component.js
@@ -4,6 +4,7 @@ console.log(StoreController);
 
 angular
     .module('app.store', [])
+<<<<<<< HEAD:front/components/store/component.js
     .controller('StoreController', [StoreController]);
 
 export var view = {
@@ -11,3 +12,11 @@ export var view = {
     templateUrl: "/components/store/view.html",
     controller: "StoreController as store"
 };
+=======
+    .controller('StoreController', StoreModule.controller);
+
+// export var view = {
+//     templateUrl: "/components/store/view.html",
+//     controller: "StoreController as store"
+// };
+>>>>>>> c362853034ba84e34e665a09b66ffa8e9e824ee1:front/components/store/store.js
