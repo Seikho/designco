c16e27de51c616777e7dcccdbd633d2637f824e4
Carl <carl@carlossus.com>
2015-06-28 21:04:23 +0800

    Deleted app.store. Moved view to screen/view.html

--- front/components/screens/controller.js
+++ front/components/screens/controller.js
@@ -1,7 +1,14 @@
 export class ScreenController {
     constructor($http) {
-        this.screens = [];
-        
+
+        //TODO: Seed this from API
+        this.screens = [{
+            { id: 1, title: "Manager's special "},
+            { id: 2, title: "4x4" },
+            { id: 3, title: "Air Conditioning" },
+            { id: 4, title: "Turbo Diesel" },
+            { id: 5, title: "Automatic" }
+        }];
     }
 }
 
