b9dc45d38ad73422fcf2b6ba3fce648585c3b53a
Carl <carl@carlossus.com>
2015-06-28 14:29:48 +0800

    Removing comments and prepend-commas from app.js

--- front/components/app.js
+++ front/components/app.js
@@ -1,13 +1,11 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-// import ngNewRouter from 'angular-new-router';
 import ngMaterial from 'angular-material';
 import * as Store from "./store/store";
 import * as Nav from "./nav/nav";
 
 var appModules = [
     "ui.router",
-    // "ngNewRouter",
     "ngMaterial",
     "app.store",
     "app.nav"
@@ -15,54 +13,34 @@ var appModules = [
 
 angular
     .module('app', appModules)
-    .config(function($stateProvider, $urlRouterProvider) {
+    .config(($stateProvider, $urlRouterProvider) =>
         $urlRouterProvider.otherwise('/state1');
 
         $stateProvider
             .state('state1', {
-                url: '/state1'
-            ,   templateUrl: '/front/components/state1.html'
+                url: '/state1',
+                templateUrl: '/front/components/state1.html'
             })
             .state('state2', {
-                url: '/state2'
-            ,   templateUrl: '/front/components/state2.html'
+                url: '/state2',
+                templateUrl: '/front/components/state2.html'
             })
             .state('store', {
                 url: '/store'
-            ,   templateUrl: '/front/components/store/store.html'
-            ,   controller: 'StoreController as store'
+                templateUrl: '/front/components/store/store.html',
+                controller: 'StoreController as store'
             })
     })
     .config($mdThemingProvider => themeHandler($mdThemingProvider));
 
 function themeHandler(provider) {
-    var designCoHue = {
+    var designcoHue = {
         "500": "f9f213"
     };
-    var designcoPalette = provider.extendPalette("yellow", designCoHue);
+    var designcoPalette = provider.extendPalette("yellow", designcoHue);
     provider.definePalette("designco", designcoPalette);
 
     provider
         .theme("default")
         .primaryPalette("designco");
 }
-
-//
-// .controller("AppController", ["$router", AppController])
-//
-// AppController.$routeConfig = [
-//     {
-//         path: "/",
-//         redirectTo: "/store"
-//     },
-//     {
-//         path: "/store",
-//         components: {
-//             left: "nav",
-//             right: "store"
-//         }
-//     }
-// ];
-//
-// function AppController($router) {
-// }
