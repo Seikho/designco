863eac2cb5d21dde8968a441c4370c882b3fe615
Carl <carl@carlossus.com>
2015-06-28 21:22:37 +0800

    Changed a few more single quotes to double quotes

--- front/components/nav/controller.js
+++ front/components/nav/controller.js
@@ -4,11 +4,11 @@ export class NavController {
 
         this.menuItems = [
             { title: "Screens", view: "screen" },
-            { title: 'Banners', view: "banner" },
-            { title: 'Vehicles', view: "vehicle" },
-            { title: 'Print', view: "print" },
-            { title: 'Fabricated', view: "fabricate" },
-            { title: 'Traditional', view: "traditional" }
+            { title: "Banners", view: "banner" },
+            { title: "Vehicles", view: "vehicle" },
+            { title: "Print", view: "print" },
+            { title: "Fabricated", view: "fabricate" },
+            { title: "Traditional", view: "traditional" }
         ];
 
 		this.currentView = this.menuItems[0];
