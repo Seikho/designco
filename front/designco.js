91d8cbc2b9bf308b89c574c5b8c05f73624744e1
Carl <carl@carlossus.com>
2015-07-07 18:31:19 +0800

    Added angular+ui+router type defs. frontend app entry point now imports the references.

--- front/designco.js
+++ front/designco.js
@@ -1,6 +1,9 @@
-import angular from "angular";
-import uiRouter from "angular-ui-router";
-import ngMaterial from "angular-material";
+/// <reference path="../src/typings/angularjs/angular.d.ts" />
+/// <reference path="../src/typings/angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="../src/typings/angular-material/angular-material.d.ts" />
+import * as angular from "angular";
+import * as uiRouter from "angular-ui-router";
+import * as ngMaterial from "angular-material";
 import CartService from "./components/cart/service";
 
 var dependencies = [
@@ -8,8 +11,8 @@ var dependencies = [
     "ngMaterial"
 ];
 
-var module = angular
+var app = angular
     .module("app", dependencies)
     .factory("cartService", CartService);
 
-export { module as default }
+export { app as default }
