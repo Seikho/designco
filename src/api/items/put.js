3eb33b3188a0b99bbb537d31461c6242cf366c01
Carl Winkler <carl@carlossus.com>
2015-07-08 21:07:56 +0800

    Added items.put to api:

---
+++ src/api/items/put.js
@@ -0,0 +1,19 @@
+var db = require("../../store/db");
+var enums = require("../../types/enums");
+var ActiveState = enums.ActiveState;
+var ItemType = enums.ItemType;
+function put(item) {
+    if (!isValidItem(item))
+        return Promise.reject("Invalid item: Require fields missing");
+    return db("items")
+        .insert(item)
+        .then(function (newIds) { return Promise.resolve(newIds[0]); });
+}
+function isValidItem(item) {
+    delete item.id;
+    item.itemState = ActiveState[ActiveState.Active];
+    var isValidType = !!ItemType[item.itemType];
+    var isValidDesc = item.description.length > 5;
+    return isValidType && isValidDesc;
+}
+module.exports = put;
