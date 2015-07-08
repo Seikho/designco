5c98a70870cb05ce655237da8206869c6b842ed8
Carl Winkler <carl@carlossus.com>
2015-07-08 20:59:04 +0800

    items.get: now returns a single item object instead of an array with one element

--- src/api/items/get.js
+++ src/api/items/get.js
@@ -1,8 +1,11 @@
 var db = require("../../store/db");
+var Promise = require("bluebird");
 function get(itemId) {
     var request = db("items").select();
     if (itemId)
-        request.where({ id: itemId });
+        request
+            .where({ id: itemId })
+            .then(function (items) { return Promise.resolve(items[0]); });
     return request;
 }
 module.exports = get;
--- src/api/items/get.ts
+++ src/api/items/get.ts
@@ -5,7 +5,9 @@ export = get;
 function get(itemId?: number) {
 	var request = db("items").select();
 	
-	if (itemId) request.where({id: itemId});
-	
+	if (itemId) request
+		.where({id: itemId})
+		.then(items => Promise.resolve(items[0]));
+		
 	return request;
 }
