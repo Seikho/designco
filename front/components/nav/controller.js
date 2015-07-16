515558334b280c20af2ad12d7830416ab0010a19
Carl <carl@carlossus.com>
2015-07-16 22:58:41 +0800

    Formatting

--- front/components/nav/controller.js
+++ front/components/nav/controller.js
@@ -1,20 +1,20 @@
 export class NavController {
-	constructor() {
-		this.self = this;
+        constructor() {
+                this.self = this;
 
-        this.menuItems = [
-            { title: "Screens", view: "screen" },
-            { title: "Banners", view: "banner" },
-            { title: "Vehicles", view: "vehicle" },
-            { title: "Print", view: "print" },
-            { title: "Fabricated", view: "fabricate" },
-            { title: "Traditional", view: "traditional" }
-        ];
+                this.menuItems = [
+                        { title: "Screens", view: "screen" },
+                        { title: "Banners", view: "banner" },
+                        { title: "Vehicles", view: "vehicle" },
+                        { title: "Print", view: "print" },
+                        { title: "Fabricated", view: "fabricate" },
+                        { title: "Traditional", view: "traditional" }
+                ];
 
-		this.currentView = this.menuItems[0];
+                this.currentView = this.menuItems[0];
 
-		this.changeView = newView => {
-			this.currentView = newView;
-		}
-	}
+                this.changeView = newView => {
+                        this.currentView = newView;
+                }
+        }
 }
