a121e135ff45afa7149f9012e60c2336c98d6cef
Carl <carl@carlossus.com>
2015-07-20 18:36:08 +0800

    jspm/systemjs and typescript type defs don't play well together. removing front end type defs

--- front/designco.js
+++ front/designco.js
@@ -1,9 +1,6 @@
-/// <reference path="../src/typings/angularjs/angular.d.ts" />
-/// <reference path="../src/typings/angular-ui-router/angular-ui-router.d.ts" />
-/// <reference path="../src/typings/angular-material/angular-material.d.ts" />
-import * as angular from "angular";
-import * as uiRouter from "angular-ui-router";
-import * as ngMaterial from "angular-material";
+import angular from "angular";
+import uiRouter from "angular-ui-router";
+import ngMaterial from "angular-material";
 import CartService from "./components/cart/service";
 
 var dependencies = [
