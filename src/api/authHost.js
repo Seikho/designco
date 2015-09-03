ebeb8f6147916734edd8cf3f445d6d661e528636
Carl <carl@carlossus.com>
2015-09-03 17:50:40 +0800

    remove api/authHost

--- src/api/authHost.js
+++
@@ -1,6 +0,0 @@
-var cfg = require("ls-config");
-function authHost() {
-    return "http://localhost:" + cfg.config("authPort") + "/register";
-}
-module.exports = authHost;
-//# sourceMappingURL=authHost.js.map
--- src/api/authHost.ts
+++
@@ -1,6 +0,0 @@
-import cfg = require("ls-config");
-export = authHost;
-
-function authHost() {
-    return "http://localhost:" + cfg.config("authPort") + "/register";
-}
