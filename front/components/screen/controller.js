0faedaa17a7b605458af9c34d1d66a07f7c07a4e
Carl <carl@carlossus.com>
2015-09-06 22:24:32 +0800

    Comment out old ng ScreenController code

--- front/components/screen/controller.js
+++ front/components/screen/controller.js
@@ -1,21 +1,5 @@
 var ScreenController = (function () {
-    function ScreenController(cart) {
-        var _this = this;
-        this.orders = [];
-        this.getOrders = function () {
-            console.log(_this.cart.orders);
-            _this.orders = _this.cart.orders;
-        };
-        this.screens = [
-            { id: 1, title: "Manager's special " },
-            { id: 2, title: "4x4" },
-            { id: 3, title: "Air Conditioning" },
-            { id: 4, title: "Turbo Diesel" },
-            { id: 5, title: "Automatic" }
-        ];
-        this.cart = cart;
-        this.orders = cart.orders; // not updating
-        //TODO: Seed this from API        
+    function ScreenController() {
     }
     return ScreenController;
 })();
--- front/components/screen/controller.ts
+++ front/components/screen/controller.ts
@@ -1,24 +1,24 @@
 export class ScreenController {
-    constructor(cart) {
-        this.cart = cart;
-        this.orders = cart.orders; // not updating
+    // constructor(cart) {
+    //     this.cart = cart;
+    //     this.orders = cart.orders; // not updating
 
-        //TODO: Seed this from API        
-    }
+    //     //TODO: Seed this from API        
+    // }
     
-    cart: App.CartService;
-    orders: Array<any> = [];
 
-    getOrders = () => {
-        console.log(this.cart.orders);
-        this.orders = this.cart.orders;
-    }
+    // orders: Array<any> = [];
 
-    screens = [
-        { id: 1, title: "Manager's special " },
-        { id: 2, title: "4x4" },
-        { id: 3, title: "Air Conditioning" },
-        { id: 4, title: "Turbo Diesel" },
-        { id: 5, title: "Automatic" }
-    ];
+    // getOrders = () => {
+    //     console.log(this.cart.orders);
+    //     this.orders = this.cart.orders;
+    // }
+
+    // screens = [
+    //     { id: 1, title: "Manager's special " },
+    //     { id: 2, title: "4x4" },
+    //     { id: 3, title: "Air Conditioning" },
+    //     { id: 4, title: "Turbo Diesel" },
+    //     { id: 5, title: "Automatic" }
+    // ];
 }
