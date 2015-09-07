f96bdbf360a27dcd2c44ded833f965ee469a336d
Carl <carl@carlossus.com>
2015-09-07 18:09:13 +0800

    Add 'showModal' knockout binding

--- front/scripts/app.js
+++ front/scripts/app.js
@@ -1,6 +1,19 @@
 var NavVM = require("../components/nav/nav");
 var ko = require("knockout");
 var modal = require("../components/modal/component");
+var $ = require("jquery");
 ko.applyBindings(new NavVM());
 ko.components.register("modal", modal);
+ko.bindingHandlers["showModal"] = {
+    init: function () { },
+    update: function (element, valueAccessor) {
+        var value = valueAccessor();
+        if (ko.utils.unwrapObservable(value)) {
+            $(element)["modal"]('show');
+            $("input", element).focus();
+        }
+        else
+            $(element)["modal"]('hide');
+    }
+};
 //# sourceMappingURL=app.js.map
--- front/scripts/app.ts
+++ front/scripts/app.ts
@@ -1,6 +1,20 @@
 import NavVM = require("../components/nav/nav");
 import ko = require("knockout");
 import modal = require("../components/modal/component");
+import $ = require("jquery");
 
 ko.applyBindings(new NavVM());
 ko.components.register("modal", modal);
+
+ko.bindingHandlers["showModal"] = {
+    init: () => { },
+    update: (element, valueAccessor) => {
+        var value = valueAccessor();
+        if (ko.utils.unwrapObservable(value)) {
+            $(element)["modal"]('show');
+            $("input", element).focus();
+        }
+
+        else $(element)["modal"]('hide');
+    }
+};
