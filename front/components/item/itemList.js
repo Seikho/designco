9c931fbc6276e857c0613bf7f083d79538a5cb5a
Carl <carl@carlossus.com>
2015-09-08 21:35:20 +0800

    Implement temporary itemList/model/view

--- front/components/item/itemList.js
+++ front/components/item/itemList.js
@@ -1,9 +1,12 @@
 var ko = require("knockout");
 var ItemModel = require("./itemModel");
+var $ = require("jquery");
+var view = require("text!./items");
 var ItemList = (function () {
     function ItemList() {
         var _this = this;
         this.items = ko.observableArray();
+        this.itemType = ko.observable("");
         this.loadModels = function (items) {
             var itemModels = items.map(function (item) { return new ItemModel(item); });
             _this.items(itemModels);
@@ -12,11 +15,15 @@ var ItemList = (function () {
             var models = _this.items().map(function (item) { return item.saveToModel(); });
             // TODO: POST request...
         };
+        this.itemType.subscribe(function (type) {
+            $.get("/items/" + type)
+                .then(_this.items);
+        });
     }
     return ItemList;
 })();
 var component = {
-    template: "/components/screen/view.html",
+    template: view,
     viewModel: ItemList
 };
 module.exports = component;
--- front/components/item/itemList.ts
+++ front/components/item/itemList.ts
@@ -1,18 +1,25 @@
 import ko = require("knockout");
 import ItemModel = require("./itemModel");
 import $ = require("jquery");
+var view = require("text!./items")
 export = component;
 
 class ItemList {
-    constructor(){}
-    
+    constructor() {
+        this.itemType.subscribe(type => {
+            $.get(`/items/${type}`)
+                .then(this.items);
+        });
+    }
+
     items = ko.observableArray<ItemModel>();
-    
+    itemType = ko.observable("");
+
     loadModels = (items: Array<App.Item>) => {
         var itemModels = items.map(item => new ItemModel(item));
         this.items(itemModels);
     }
-    
+
     saveModels = () => {
         var models = this.items().map(item => item.saveToModel());
         // TODO: POST request...
@@ -20,6 +27,6 @@ class ItemList {
 }
 
 var component = {
-    template: "/components/screen/view.html",
+    template: view,
     viewModel: ItemList
 };
