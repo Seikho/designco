bd893d0aa394b8225f6970ef2dd08f32023e4b2b
Carl Winkler <carl.winkler86@gmail.com>
2015-09-11 21:43:06 +0800

    Update ko-lists lib

--- front/scripts/libs/ls-ko-lists.js
+++ front/scripts/libs/ls-ko-lists.js
@@ -18,7 +18,11 @@ var List = (function () {
             return $.post(_this.options.url, { created: created, updated: updated, deleted: deleted });
         };
         this.loadModels = function (models) {
-            var newModels = models.map(function (model) { return _this.options.createModel(model); });
+            var newModels = models.map(function (model) {
+                var newModel = _this.options.createModel();
+                newModel.loadModel(model);
+                return newModel;
+            });
             _this.models(newModels);
         };
         this.saveToModels = function () {
@@ -39,14 +43,16 @@ var List = (function () {
 })();
 exports.List = List;
 var Model = (function () {
-    function Model(model) {
+    function Model() {
         var _this = this;
+        this.modelKeys = [];
+        this.deletedFlag = ko.observable(false);
         /**
          * Fallback when a custom loadModel function is not provided
          */
         this.loadModel = function (model) {
-            Object.keys(model)
-                .forEach(function (key) {
+            _this.modelKeys = Object.keys(model);
+            _this.modelKeys.forEach(function (key) {
                 _this.modelKeys.push(key);
                 _this[key] = ko.observable(model[key]);
             });
@@ -63,6 +69,8 @@ var Model = (function () {
         };
         this.isCreated = ko.computed(function () {
             if (_this.originalModel == null)
+                return true;
+            if (_this.modelKeys == null)
                 return true;
             if (_this.modelKeys.length === 0)
                 return true;
@@ -88,12 +96,6 @@ var Model = (function () {
             return areAllOriginal;
         });
         this.isDeleted = ko.computed(function () { return _this.deletedFlag(); });
-        this.deletedFlag = ko.observable(false);
-        this.modelKeys = [];
-        if (!model)
-            return;
-        this.originalModel = model;
-        this.loadModel(model);
     }
     return Model;
 })();
