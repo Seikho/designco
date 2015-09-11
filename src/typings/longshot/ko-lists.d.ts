e11b0df5b3adb117ea6f784ea98fd60233c3554c
Carl <carl@carlossus.com>
2015-09-11 19:26:36 +0800

    Add ls-ko-lists to type defs

---
+++ src/typings/longshot/ko-lists.d.ts
@@ -0,0 +1,60 @@
+/// <reference path="../knockout/knockout.d.ts" />
+/// <reference path="../jquery/jquery.d.ts" />
+
+declare module "ls-ko-lists" {
+    class List<T extends ModelViewModel> implements ListViewModel {
+        new(options: ListOptions);
+        options: ListOptions;
+        models: KnockoutObservableArray<ModelViewModel>
+
+        loadFromServer(): void;
+        saveToServer(): any;
+
+        loadModels(models: any[]): void;
+        saveToModels(): any;
+        
+        deleteModel(model: ModelViewModel);
+        unDeleteModel(model: ModelViewModel);
+    }
+    
+    interface ListViewModel {
+        loadFromServer(): void;
+        saveToServer(): any;
+
+        loadModels(models: any[]): void;
+        saveToModels(): any;
+        
+        deleteModel(model: ModelViewModel);
+        unDeleteModel(model: ModelViewModel);
+    }
+    
+    class Model implements ModelViewModel {
+        new(model: any);
+        
+        originalModel: any;
+        loadModel(model: any): void;
+        saveToModel(): any;
+        
+        isCreated: KnockoutComputed<boolean>;
+        isUpdated: KnockoutComputed<boolean>;
+        isDeleted: KnockoutComputed<boolean>;
+        
+        deletedFlag: KnockoutObservable<boolean>;
+    }
+
+    interface ModelViewModel {
+        loadModel(model: any): void;
+        saveToModel(): any;
+        
+        isCreated: KnockoutComputed<boolean>;
+        isUpdated: KnockoutComputed<boolean>;
+        isDeleted: KnockoutComputed<boolean>;
+        
+        deletedFlag: KnockoutObservable<boolean>;
+    }
+
+    interface ListOptions {
+        url: string;
+        createModel: (model?: any) => ModelViewModel;
+    }
+}
